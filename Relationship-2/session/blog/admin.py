ee588dc41ae5925d8342c670f2588d798ae9af8f
SYunje <118589494+SYunje@users.noreply.github.com>
2023-04-13 23:53:04 +0900

    Add files via upload

---
+++ Relationship-2/session/blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+# TODO: BlogAdmin 변경, Comment 등록, Tag 등록
+admin.site.register(Blog)
+admin.site.register(Comment)
+admin.site.register(TTag)
